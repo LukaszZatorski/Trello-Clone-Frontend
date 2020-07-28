6937a5b9e51ec7af6fc7d1e01681d1e4eefe5c93
Zatorski <lukasz.zatorskii@gmail.com>
2020-07-28 14:52:20 +0200

    Redirect to Landing Page after logout

--- src/components/LogOut/index.tsx
+++ src/components/LogOut/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import apiClient from '../../services/apiClient';
+import { useHistory } from 'react-router-dom';
 
 type LogOutProps = {
   setLoggedIn: (x: boolean) => void;
 };
 
 const LogOut = ({ setLoggedIn }: LogOutProps) => {
+  let history = useHistory();
   const logout = () => {
     apiClient.post('/logout').then((response) => {
       if (response.status === 204) {
         setLoggedIn(false);
         sessionStorage.removeItem('loggedIn');
         sessionStorage.removeItem('email');
+        history.push('/');
       }
     });
   };
