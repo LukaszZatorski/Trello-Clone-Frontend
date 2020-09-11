215681cf14179877000778f991e936ab36f21d96
Zatorski <lukasz.zatorskii@gmail.com>
2020-09-11 15:15:21 +0200

    Whitelist bg-x-700 colors in purgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  purge: [
-    'src/**/*.js',
-    'src/**/*.jsx',
-    'src/**/*.ts',
-    'src/**/*.tsx',
-    'public/**/*.html',
-  ],
+  purge: {
+    content: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      'public/**/*.html',
+    ],
+    options: {
+      whitelist: [
+        'bg-yellow-700',
+        'bg-green-700',
+        'bg-red-700',
+        'bg-purple-700',
+        'bg-pink-700',
+      ],
+    },
+  },
   theme: {
     extend: {},
   },
